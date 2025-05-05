48f8c974bfa0e2dde59059c6a649a41f6617bdb9
rosali Johansson <Rosali.alfta@gmail.com>
2025-05-05 13:22:24 +0200

    created angular component for navigation menu

---
+++ src/app/navmenu/navmenu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navmenu',
+  imports: [],
+  templateUrl: './navmenu.component.html',
+  styleUrl: './navmenu.component.css'
+})
+export class NavmenuComponent {
+
+}
